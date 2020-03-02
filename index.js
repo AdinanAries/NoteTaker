d95f2d847dc3179876a2475122d989b24ab14659
Mohammed Adinan <adinanaries@outlook.com>
2020-03-02 17:55:04 -0500

    added UI elements and styled to desired output

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require("express");
 const path = require("path");
-const app = express();
-
+const uuid = require("uuid");
 const fs = require("fs");
+
+const app = express();
 
 var Notes;
 fs.readFile("./notes/db.json", "utf8", function(err, contents) {
   Notes = JSON.parse(contents);
+  console.log(Notes);
+  //console.log(JSON.stringify(Notes));
 });
 
 //route for getting notes.html
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+//middleware parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//adding static assets
+app.use(express.static(path.join(__dirname, "staticAssets")));
+
+//route to get my css file
+app.get("/styles/style.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/styles", "style.css"));
+});
+
+//route for getting my js file
+app.get("/scripts/script.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/scripts", "app.js"));
 });
 
 //route for getting index.html
@@ -21,20 +41,37 @@ app.get("/", (req, res) => {
 
 //API route for getting notes
 app.get("/api/notes", (req, res) => {
-  res.json(Notes);
+  res.json(JSON.stringify(Notes));
 });
 
 //API route for adding notes
 //This endpoint to take just notes as string and generate the note ID using a third party library
 app.post("/api/notes", (req, res) => {
-  //code to generate note ID
-
-  var newNote = req.body();
-  //returns ID plus added notes to client side for easy manipulation of data
+  const newNoteID = uuid.v4(); //new id for newly added note
+  var newNoteText = req.body.note; // actual note content from ajax post request
+  console.log(newNoteID);
+  console.log(newNoteText);
+  //adding new note to Notes
+  Notes[newNoteID] = newNoteText;
+  res.json(JSON.stringify(Notes));
+  //write code that updates or replaces existing db.json on the file system
 });
 
 //API route to delet note
-app.delete("/api/notes/:id", (req, res) => {});
+app.delete("/api/notes/:id", (req, res) => {
+  //checking is this note exists
+  NoteID = req.params.id;
+  //checking is provided id exist for Notes Object
+  const found = Notes.hasOwnProperty(NoteID);
+
+  if (found) {
+    delete Notes[NoteID];
+    res.json(JSON.stringify(Notes));
+    //write code that replaces or updates existing db.json file
+  } else {
+    res.status(400).json({ msg: "There isn't any note with provided id" });
+  }
+});
 
 const PORT = 5000;
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,20 +2,18 @@
 function getNotes() {
   $.ajax({
     type: "GET",
-    url: "http://localhost:5000/api/notes",
+    url: "/api/notes",
     success: function(res) {
-      console.log(res);
       alert(res);
-      //parsing json response for individual data parts collection
-      const data = JSON.parse(res);
-      alert(data);
+      data = JSON.parse(res);
+      alert(data.firstObject);
     }
   });
 }
 
 getNotes();
 
-//function to add new note
+/*/function to add new note
 //funcion takes an object comprising of notes and id
 function saveNote(data) {
   var noteId = data.id;
@@ -23,25 +21,26 @@ function saveNote(data) {
 
   $.ajax({
     type: "POST",
-    url: "http://localhost:5000/api/notes",
+    url: "/api/notes",
     data: `id=${noteId}&note=${noteNotes}`,
     success: function(res) {
       alert("Note Saved Successfully!");
       alert(res);
     }
   });
-}
+}*/
 
 //function to add new note
 //funcion takes just a string of note text to be save in the db.json
-function saveNote(note) {
+function saveNote(noteText) {
   $.ajax({
     type: "POST",
-    url: "http://localhost:5000/api/notes",
-    data: `note=${note}`,
+    url: "/api/notes",
+    data: { note: noteText },
     success: function(res) {
       alert("Note Saved Successfully!");
       alert(res);
     }
   });
 }
+//saveNote("This is another note");
